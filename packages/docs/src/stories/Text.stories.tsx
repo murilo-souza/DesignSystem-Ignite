02791063fad16dc93122209134663d924b0d4c9e
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-01-03 22:35:31 -0300

    update

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum sunt harum consectetur dolorum aliquid velit, alias officiis animi atque maiores, debitis nostrum unde, autem et dolore voluptate commodi repellendus eaque.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
---
+++ packages/react/src/components/Checkbox/styles.ts
@@ -0,0 +1,7 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
+
+import { styled } from '../../styles'
+
+export const CheckboxContainer = styled(Checkbox.Root, {})
+
+export const CheckboxIndicator = styled(Checkbox.Indicator, {})
